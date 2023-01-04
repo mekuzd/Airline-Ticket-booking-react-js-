c5cfdb3195e21fe8ad6330eb27e432dc3d15a7ac
Mekuzd <emekaseun.es@gmail.com>
2023-01-04 11:15:20 +0100

    home page progree

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import { react } from "react";
+import BookTabs from "./Components/BookTabs";
+
 import DefaultLayout from "./Layouts/DefaultLayouts";
 
 function App() {
   return (
     <>
       <DefaultLayout />
+      <section className="section ">
+        <main className="row justify-content-center p-3 align-items-center">
+          <div className="col-sm-6 p-2 ">
+            <h1 className="  travel ">Travel to new destinations with us.</h1>
+            <h4>Explore the world together</h4>
+            <button className="btn btn-outline-info mt-5 ">Book Now</button>
+          </div>
+          <div className="col-sm-4 mt-5 py-5 ">
+            <img src="../airplane-flight.webp" alt="" className="planeImg " />
+          </div>
+        </main>
+        <BookTabs />
+      </section>
     </>
   );
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -19,13 +19,10 @@ const Navbar = () => {
   const handleClose = () => setShow(false);
 
   return (
-    <nav
-      className={`d-flex  shadow justify-content-around align-items-center p-2 `}
-    >
+    <nav className="shadow">
       {/* mobile navbar  */}
-      <div className="m-2">
-        <h4>Mekuz Airline</h4>
-      </div>
+
+      <h4>Mekuz Airline</h4>
 
       <button className="d-block d-md-none ms-auto barsbtn ">
         <FaBars className="bars" onClick={handleShow} />
@@ -41,14 +38,14 @@ const Navbar = () => {
 
       {/* nav links on desktop */}
       <div className="d-none d-md-flex  justify-content-center ">
-        <li className="links mx-4 fs-6">BookNow</li>
-        <li className="links mx-4 fs-6">Explore </li>
+        <li className="links mx-4 fs-6 ">BookNow</li>
+        <li className="links mx-4 fs-6 ">Explore </li>
         <li
-          className="links ms-2 fs-6 position-relative"
+          className="links ms-2 fs-6 position-relative "
           onMouseOver={showDropdown}
           onMouseOut={closeDropdown}
         >
-          Aboutus{" "}
+          About us{" "}
           <span className="mx-1">
             {dropdown ? <FaAngleUp /> : <FaAngleDown />}
           </span>
@@ -57,8 +54,8 @@ const Navbar = () => {
       </div>
 
       <div className="d-none d-md-flex justify-content-center align-items-center">
-        <li className="links fs-5">SignUp</li>
-        <li className="mx-4 links fs-5 ">
+        <li className="links fs-5 ">SignUp</li>
+        <li className="mx-4 links fs-5  ">
           Login{" "}
           <span>
             <AiOutlineLogin />
