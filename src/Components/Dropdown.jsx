4a2cc588596a062e5e4421e740ffc35f1323c3da
Mekuzd <emekaseun.es@gmail.com>
2023-01-07 00:05:00 +0100

    worked on app.jsx && index.css

--- src/Components/Dropdown.jsx
+++ src/Components/Dropdown.jsx
@@ -2,9 +2,9 @@ const Dropdown = () => {
   return (
     <div className="card   card-body ps-0">
       <ul>
-        <li className="links">Contacts</li>
-        <li className="links">Team</li>
-        <li className="links">Testimonials</li>
+        <li className="dropdown">Contacts</li>
+        <li className="dropdown">Team</li>
+        <li className="dropdown">Testimonials</li>
       </ul>
     </div>
   );
